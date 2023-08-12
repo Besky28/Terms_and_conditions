c23800eeb3d28828aa7bdf50b59253e829b94972
Besky28 <tobyloba28@gmail.com>
2023-08-12 09:53:06 +0100

    updated

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function toggleSidebar() {
+ function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     if (sidebar.style.display === 'none') {
       sidebar.style.display = 'block';
     } else {
       sidebar.style.display = 'none';
     }
-  }
-  
+ }
